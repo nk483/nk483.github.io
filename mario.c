1dd41b35be44d6b9db5c50eba51278227c4e3549
nk483 <nk483@users.noreply.github.com>
2019-09-15 16:53:00 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -4,26 +4,35 @@
 int main(void)
 {
     int height = 0;
-    int maxheight = 8;
-    do {
-       height = get_int("Input an integer from 1-8 for the pyramid's height: ");
+    //Gets height as input from user
+    do 
+    {
+        height = get_int("Input an integer from 1-8 for the pyramid's height: ");
     }
-    while(!(height>0 && height<9));
+    //Condition to make sure input is from 1-8, otherwise it re-asks user for height
+    while (!(height > 0 && height < 9));
+    int dummyheight = height;
     int length = 4;
-    for(int i = 0;i<height;i++) {
-        for(int k = 0; k<maxheight;k++) {
-           printf(" ") ;
+    for (int i = 0; i < height; i++) 
+    {
+        //This loop is so that the hashtags have space to fan out to the left
+        for (int k = 0; k < dummyheight-1; k++) 
+        {
+            printf(" ") ;
         }
-            for(int j=0; j<length; j++) { 
-                if (j == length/2-1) {
-                    printf("  ");
-                    j+=2;
-                }
-                printf("#");
+        for (int j = 0; j < length; j++) 
+        { 
+            //This if statement is to implement the gap in middle of each pyramid line
+            if (j == length / 2 - 1) 
+            {
+                printf("  ");
+                j += 2;
             }
+            printf("#");
+        }
         printf("\n");
-        length = length+2;
-        maxheight--;
+        length = length + 2;
+        dummyheight--;
     }
     
 }
