be90452fb74c3535166ec68c2744830235998c8c
nk483 <nk483@users.noreply.github.com>
2019-09-15 19:19:31 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- credit.c
+++ credit.c
@@ -4,6 +4,7 @@ int main(void)
 {
     // Prompts user for card number
     long number = get_long("Number: ");
+    // Setting variables to the value of the number so I can make changes without changing the original value
     long copy = number;
     long secondcopy = number;
     long thirdcopy = number;
@@ -12,23 +13,24 @@ int main(void)
     int digit = 0;
     int length = 0;
     bool checksum = false;
+    //This loop does the first part of the checksum computation
     while (copy > 0) 
     {
         copy = copy / 10;       
-        digit = 2*(copy % 10);
+        digit = 2 * (copy % 10);
         sum += digit % 10;
         sum += digit / 10;    
-        copy = copy/10;        
+        copy = copy / 10;        
     }
-    
+    //This loop does the second part of the checksum computation
     while (secondcopy > 0)
     {
         digit = secondcopy % 10;
         sum += digit % 10;
         sum += digit / 10;    
-        secondcopy = secondcopy/100;
+        secondcopy = secondcopy / 100;
     } 
-    printf("%i\n",sum);
+    // This loop does the final part of the checksum computation
     if (sum % 10 == 0) 
     {
         checksum = true;
@@ -37,16 +39,18 @@ int main(void)
     {
         thirdcopy = thirdcopy / 10;
     }
+    //Counts the number of digits the number has
     while (fourthcopy > 0) 
     {
         fourthcopy = fourthcopy / 10;
         length++;
     }
+    //This condition says if the card starts with 34 or 37, passes the checksum, and has 15 digits, then it is an American Express card
     if ((thirdcopy == 34 || thirdcopy == 37) && checksum && length == 15) 
     {
         printf("AMEX\n");
     }
-    else if ((thirdcopy == 51 || thirdcopy == 52 || thirdcopy == 53 || thirdcopy == 54 || thirdcopy == 55)&& checksum && length == 16) 
+    else if ((thirdcopy == 51 || thirdcopy == 52 || thirdcopy == 53 || thirdcopy == 54 || thirdcopy == 55) && checksum && length == 16) 
     {
         printf("MASTERCARD\n");
     }
