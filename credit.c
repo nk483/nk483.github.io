56ffbe6b5df1a61303bff02e9530769ac1c796e6
nk483 <nk483@users.noreply.github.com>
2019-09-15 19:14:09 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- credit.c
+++ credit.c
@@ -2,12 +2,15 @@
 #include <stdio.h>
 int main(void) 
 {
+    // Prompts user for card number
     long number = get_long("Number: ");
     long copy = number;
     long secondcopy = number;
     long thirdcopy = number;
+    long fourthcopy = number;
     int sum = 0;
     int digit = 0;
+    int length = 0;
     bool checksum = false;
     while (copy > 0) 
     {
@@ -25,6 +28,7 @@ int main(void)
         sum += digit / 10;    
         secondcopy = secondcopy/100;
     } 
+    printf("%i\n",sum);
     if (sum % 10 == 0) 
     {
         checksum = true;
@@ -33,15 +37,20 @@ int main(void)
     {
         thirdcopy = thirdcopy / 10;
     }
-    if ((thirdcopy == 34 || thirdcopy == 37) && checksum) 
+    while (fourthcopy > 0) 
+    {
+        fourthcopy = fourthcopy / 10;
+        length++;
+    }
+    if ((thirdcopy == 34 || thirdcopy == 37) && checksum && length == 15) 
     {
         printf("AMEX\n");
     }
-    else if ((thirdcopy == 51 || thirdcopy == 52 || thirdcopy == 53 || thirdcopy == 54 || thirdcopy == 55)&& checksum) 
+    else if ((thirdcopy == 51 || thirdcopy == 52 || thirdcopy == 53 || thirdcopy == 54 || thirdcopy == 55)&& checksum && length == 16) 
     {
         printf("MASTERCARD\n");
     }
-    else if (thirdcopy / 10 == 4 && checksum) 
+    else if (thirdcopy / 10 == 4 && checksum && (length == 13 || length == 16)) 
     {
         printf("VISA\n");
     }
