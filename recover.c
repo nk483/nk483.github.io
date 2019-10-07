7c64fa2b3868afd92e89aa22ea3c6deb155e60a4
nk483 <nk483@users.noreply.github.com>
2019-10-07 03:41:24 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,52 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+
 
 
 int main(int argc, char *argv[])
 {
+    // Handles an incorrect number of command line arguments
     if (argc != 2)
     {
         printf("Correct Usage: image file name");
         return 1;
     }
+    // Handles an input file that cannot be read
     if (fopen(argv[1], "r") == NULL)
     {
         printf("File could not be read");
         return 2;
     }
-    unsigned char blockSize[512];
-    int jpegNumber = 0;
-    bool jpegMarker = true;
-    char name[20];
-    bool foundOneJpeg = false;
-
-    FILE *jpeg = NULL;
 
     FILE *file = fopen(argv[1], "r");
+    // To read the input in blocks of 512 bytes
+    unsigned char blockSize[512];
 
-
-    while (fread(blockSize, 512, 1, file) == 1)
-    {
-
-        if (blockSize[0] == 255 && blockSize[1] == 216 && blockSize[2] == 255)
-        {
-            if (foundOneJpeg)
-            {
-            fclose(jpeg);
-            }
-            foundOneJpeg = true;
-            sprintf(name, "%i.jpg", jpegNumber);
-            jpeg = fopen(name, "a");
-            jpegNumber++;
-        }
-        if (foundOneJpeg)
-        {
-        fwrite(blockSize, 512, 1, jpeg);
-        }
-
-
-    }
-
-    return 0;
 }
+
