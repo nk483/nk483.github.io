d5e2cacee03e85039894eec8288754ba48f02536
nk483 <nk483@users.noreply.github.com>
2019-10-07 04:14:15 +0000

    automated commit by submit50

--- helpers.c
+++ helpers.c
@@ -6,12 +6,15 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     double sum = 0;
     int rounded = 0;
+
     for (int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for (int j = 0; j < width; j++)
         {
+            // We simply add the R, G, and B values of pixel, then average
             sum = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed;
             sum = sum / 3;
+            // To convert the double to int we cast it
             rounded = (int) round(sum);
             image[i][j].rgbtBlue = rounded;
             image[i][j].rgbtGreen = rounded;
@@ -38,7 +41,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         }
     }
 
-     for (int i = 0; i < height; i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
@@ -67,10 +70,12 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            // This is an inner nested loop as for both the rows and columns we based the value off of pixels -1, 0, 1 away
             for (int k = -1; k < 2; k++)
             {
                 for (int l = -1; l < 2; l++)
                 {
+                    // Checks If the array access would be out of bounds and skips the value call if so, also lessens divisor by 1 since you are average less values
                     if ((j + l < 0) || (j + l >= width))
                     {
                         divisor--;
@@ -82,9 +87,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     }
                     else
                     {
-                    blueSum += image[i + k][j + l].rgbtBlue;
-                    redSum += image[i + k][j + l].rgbtRed;
-                    greenSum += image[i + k][j + l].rgbtGreen;
+                        blueSum += image[i + k][j + l].rgbtBlue;
+                        redSum += image[i + k][j + l].rgbtRed;
+                        greenSum += image[i + k][j + l].rgbtGreen;
                     }
                 }
             }
@@ -94,6 +99,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             dummy[i][j].rgbtBlue = blueAverage;
             dummy[i][j].rgbtRed = redAverage;
             dummy[i][j].rgbtGreen = greenAverage;
+            // resets the values of the loop
             blueSum = 0;
             redSum = 0;
             greenSum = 0;
@@ -105,6 +111,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
+            // Updates image at the end
             image[i][j].rgbtBlue = dummy[i][j].rgbtBlue;
             image[i][j].rgbtRed = dummy[i][j].rgbtRed;
             image[i][j].rgbtGreen = dummy[i][j].rgbtGreen;
@@ -116,16 +123,22 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    //d
     int xGradientBlue = 0;
+    //s
     int xGradientRed = 0;
+    //a
     int xGradientGreen = 0;
+    //b
     int yGradientBlue = 0;
     int yGradientRed = 0;
     int yGradientGreen = 0;
     int singleValueBlue = 0;
     int singleValueRed = 0;
     int singleValueGreen = 0;
-    RGBTRIPLE dummy[height+2][width+2];
+    // Made the dummy array extra big to create a border of all 0s
+    RGBTRIPLE dummy[height + 2][width + 2];
+    //l
     for (int i = 0; i < height + 2; i++)
     {
         for (int j = 0; j < width + 2; j++)
@@ -135,6 +148,7 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             dummy[i][j].rgbtGreen = 0;
         }
     }
+    // x
     for (int i = 0; i < height + 2; i++)
     {
         for (int j = 0; j < width + 2; j++)
@@ -147,9 +161,9 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             }
             else
             {
-                dummy[i][j].rgbtBlue = image[i-1][j-1].rgbtBlue;
-                dummy[i][j].rgbtRed = image[i-1][j-1].rgbtRed;
-                dummy[i][j].rgbtGreen = image[i-1][j-1].rgbtGreen;
+                dummy[i][j].rgbtBlue = image[i - 1][j - 1].rgbtBlue;
+                dummy[i][j].rgbtRed = image[i - 1][j - 1].rgbtRed;
+                dummy[i][j].rgbtGreen = image[i - 1][j - 1].rgbtGreen;
             }
         }
     }
@@ -178,9 +192,9 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
             {
                 singleValueGreen = 255;
             }
-            image[i-1][j-1].rgbtBlue = singleValueBlue;
-            image[i-1][j-1].rgbtGreen = singleValueGreen;
-            image[i-1][j-1].rgbtRed = singleValueRed;
+            image[i - 1][j - 1].rgbtBlue = singleValueBlue;
+            image[i - 1][j - 1].rgbtGreen = singleValueGreen;
+            image[i - 1][j - 1].rgbtRed = singleValueRed;
         }
     }
     return;
