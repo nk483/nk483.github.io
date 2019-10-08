16bd47752c01ad79213eaabd757359a93e88ad0e
nk483 <nk483@users.noreply.github.com>
2019-10-08 01:27:36 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 
 
@@ -21,6 +22,32 @@ int main(int argc, char *argv[])
     FILE *file = fopen(argv[1], "r");
     // To read the input in blocks of 512 bytes
     unsigned char blockSize[512];
+    char *jpegName = malloc(20*sizeof(char));
+    FILE *jpeg = NULL;
+    int jpegCounter = 0;
+    bool firstJpeg = false;
+
+    while (fread(blockSize, 1, 512, file) != -1)
+    {
+        if (blockSize[0] == 255 && blockSize[1] == 216 && blockSize[2] == 255 && (blockSize[3] < 240 && blockSize[3] >= 224))
+        {
+            if (firstJpeg)
+            {
+                fclose(jpeg);
+            }
+            sprintf(jpegName,"%i.jpg",jpegCounter);
+            jpeg = fopen(jpegName,"a");
+            firstJpeg = true;
+            jpegCounter++;
+
+        }
+        if (firstJpeg)
+        {
+        fwrite(blockSize, 1, 512, jpeg);
+        }
+    }
+    free(jpegName);
+    fclose(jpeg);
 
 }
 
